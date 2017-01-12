d27026b00d35aa71233cc0aef5ddb905e3469de2
dpazel <dpazel@optonline.net>
2017-01-12 09:59:13 -0500

    Remove tests from source file in binary_search

--- searches/binary_search.py
+++ searches/binary_search.py
@@ -51,30 +51,3 @@ def find_lower(alist, item):
             first = midpoint + 1
 
     return found
-
-
-def ordered_dict_example():
-    d = {}
-    d[10] = 100
-    d[5] = 20
-    d[7] = 70
-    d[2] = 50
-    od = OrderedDict(sorted(d.items(), key=lambda t: t[0]))
-    print od
-    print type(od.keys())
-    for k in od.keys():
-        print k
-
-    x = [Fraction(2, 3), Fraction(1, 2)]
-    sorted(x)
-
-
-if __name__ == '__main__':
-    lisst = [1, 7, 9, 14, 23, 80]
-    lisst[find_lower(lisst, 80)]
-    for i in range(1, 81):
-        x = find_lower(lisst, i)
-        if x != -1:
-            print '{0} answer={1}'.format(i, lisst[x])
-
-    ordered_dict_example()
